0b45a4ef62b5daf3ce50482cb69c5e2f7ea6c62b
Sebastian Rojas Muñoz <90258827+SRojastxt@users.noreply.github.com>
2021-10-30 12:00:38 -0500

    hola

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://www.facebook.com/profile.php?id=100067996269330">Sebastian Rojas</a>
+    </div>
+    <div>
+      <a href="https://www.instagram.com/sebas_unu/">Sigueme en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
